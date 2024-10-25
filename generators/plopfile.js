85cd2b81291078ee402c074371dc18a5b47c1e2f
João Vitor <jvitordiass@outlook.com>
2024-10-25 17:07:17 -0300

    chore: Configures MUI Design in plop templates
      Updates plopfile.ts to add a new component exports in the index file

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 module.exports = (plop) => {
   plop.setGenerator('component', {
     description: 'Create a component',
@@ -16,8 +19,28 @@ module.exports = (plop) => {
       },
       {
         type: 'add',
-        path: '../src/app/components/{{pascalCase name}}/{{kebabCase name}}.test.tsx',
-        templateFile: 'templates/test.tsx.hbs'
+        path: '../src/app/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      },
+      {
+        type: 'append',
+        path: '../src/app/components/index.ts',
+        template: "export * from './{{pascalCase name}}'",
+        separator: ''
+      },
+      () => {
+        const indexPath = path.resolve(
+          __dirname,
+          '../src/app/components/index.ts'
+        )
+        const content = fs.readFileSync(indexPath, 'utf-8')
+        const lines = content.split('\n')
+        const updatedContent =
+          lines
+            .filter((line) => line.trim() !== '')
+            .sort()
+            .join('\n') + '\n'
+        fs.writeFileSync(indexPath, updatedContent)
       }
     ]
   })
